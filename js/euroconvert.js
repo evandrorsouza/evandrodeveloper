481e064aaad636d8e7f1f498b650712786909050
Evandro Souza <evandrorobertodesouza@gmail.com>
2021-10-02 14:30:51 -0300

    Ajustes conversor moeda

--- js/euroconvert.js
+++ js/euroconvert.js
@@ -4,7 +4,39 @@ function convert(){
     
     var real = euro * euroemreal;  //(cel * 9/5) + 32;
     
-    document.getElementById("real").innerHTML = real.toFixed(2);
-    
+    document.getElementById("real").innerHTML = real.toFixed(2);    
     
     }
+
+    function pegar_cotacao(callback){
+
+        $.ajax({
+    
+            url: "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                callback(data);
+            },
+            error: function(){
+                alert("erro na requisição");
+            }
+    
+            
+        });
+    
+    }
+    
+    function usar_cotacao(moeda){
+        
+        var euroatual = moeda.EURBRL.ask;
+        console.log(euroatual);
+    
+        var euroatualformatado = parseFloat(euroatual);
+    
+        $("#testeMoeda").html(euroatualformatado.toFixed(2));
+            
+        //document.getElementById("testeMoeda").innerHTML = euroatualformatado.toFixed(2);
+    
+    }
+    pegar_cotacao(usar_cotacao);
